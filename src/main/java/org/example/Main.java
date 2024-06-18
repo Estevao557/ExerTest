597313eaa310efca7ab16c27f0de65e7053ef85f
Estevao <rat871@hotmail.com>
2024-06-17 21:38:42 -0300

    Incluindo Mes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,7 +13,8 @@ public class Main {
         //exe4(15)
        // exe5("100");
        // exe6();
-        exe7(5);
+        //exe7(5);
+    mes(1);
     }
 
     public static void exe() {
@@ -124,4 +125,61 @@ public class Main {
         System.out.println("O resultado de " + n + " + " + n + n + " + " + n + n + n + " é: " + resultado);
 
     }
+
+    public static void mes(int mes){
+
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicitar ao usuário o mês
+        System.out.print("Digite o mês (1-12): ");
+        mes = scanner.nextInt();
+
+        // Exibir calendário para o mês escolhido
+        switch (mes) {
+            case 1:
+                System.out.println("Janeiro");
+                break;
+            case 2:
+                System.out.println("Fevereiro");
+                break;
+            case 3:
+                System.out.println("Março");
+                break;
+            case 4:
+                System.out.println("Abril");
+                break;
+            case 5:
+                System.out.println("Maio");
+                break;
+            case 6:
+                System.out.println("Junho");
+                break;
+            case 7:
+                System.out.println("Julho");
+                break;
+            case 8:
+                System.out.println("Agosto");
+                break;
+            case 9:
+                System.out.println("Setembro");
+                break;
+            case 10:
+                System.out.println("Outubro");
+                break;
+            case 11:
+                System.out.println("Novembro");
+                break;
+            case 12:
+                System.out.println("Dezembro");
+                break;
+            default:
+                System.out.println("Mês inválido");
+        }
+
+        scanner.close();
+    }
+
+
+
+
 }
